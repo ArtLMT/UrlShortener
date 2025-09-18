e2a55fece0d434e97d8cfb4ad1765c764b6ca4ec
le minh thanhe <leminhthanh280425@gmail.com>
2025-09-18 15:34:29 +0700

    Add project files.

---
+++ UrlShortener.Infrastructure/Data/UrlShortenerDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrlShortener.Infrastructure.Data
+{
+    internal class UrlShortenerDbContext
+    {
+    }
+}

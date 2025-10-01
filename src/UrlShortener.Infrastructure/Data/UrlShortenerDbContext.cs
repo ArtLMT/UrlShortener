2fea60b52ac2e6de99c013392501ebb7536619ac
phamthang44 <Ducthang4342@gmail.com>
2025-10-01 16:02:57 +0700

    fix: structure of solution

--- src/UrlShortener.Infrastructure/Data/UrlShortenerDbContext.cs
+++ src/UrlShortener.Infrastructure/Data/UrlShortenerDbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,22 @@ using System.Threading.Tasks;
 
 namespace UrlShortener.Infrastructure.Data
 {
-    internal class UrlShortenerDbContext
+    public class UrlShortenerDbContext : DbContext
     {
+        public UrlShortenerDbContext(DbContextOptions<UrlShortenerDbContext> options)
+            : base(options)
+        {
+        }
+
+        //public DbSet<ShortUrl> ShortUrls { get; set; } = null!;
+        //public DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //// Nếu bạn dùng Fluent API tách riêng config:
+            //modelBuilder.ApplyConfigurationsFromAssembly(typeof(UrlShortenerDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
